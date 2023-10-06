b8abf7902c5fca61ce39428291f4193b8a45e1ef
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-06 12:54:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // Result should be 62
 
-// Write your code here
+const random = Math.ceil(Math.random() * 100); 
+
+const num3 = 40;
+const num4 = 9;
+const mod = num3 % num4; // Remainder should be 4
+
+const numbers = [2, 5, 1, 8, 20, 7];
+const max = Math.max(...numbers); // Highest number should be 20
+
+
+console.log('Multiply:', multiply);
+console.log('Random:', random);
+console.log('Mod:', mod);
+console.log('Max:', max);
+
+
